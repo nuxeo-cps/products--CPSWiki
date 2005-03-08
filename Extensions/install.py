5d7ec6838e1fe81bb1bada2ef5e7ead830702200
ogrisel <devnull@localhost>
2005-03-08 13:39:56 +0000

    cpsworkflow import

--- Extensions/install.py
+++ Extensions/install.py
@@ -24,7 +24,7 @@ from Products.CMFCore.CMFCorePermissions import View, ModifyPortalContent
 from Products.CPSWiki.wikipermissions import addWikiPage, deleteWikiPage,\
     editWikiPage, viewWikiPage
 
-from Products.CPSCore.CPSWorkflow import \
+from Products.CPSWorkflow.transitions import \
      TRANSITION_INITIAL_PUBLISHING, TRANSITION_INITIAL_CREATE, \
      TRANSITION_ALLOWSUB_CREATE, TRANSITION_ALLOWSUB_PUBLISHING, \
      TRANSITION_BEHAVIOR_PUBLISHING, TRANSITION_BEHAVIOR_FREEZE, \
